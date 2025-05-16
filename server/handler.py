91a7ecfce125ac1bfdfe0db011c66662b57fe8cf
krisnaganesha1609 <igkganes@gmail.com>
2025-05-16 19:36:04 +0700

    fix: now handler can handle get img (jpg, png, svg only)

--- server/handler.py
+++ server/handler.py
@@ -10,8 +10,12 @@ def handleClientRequest(conn: socket, addrTuple):
 
         print(f"{bcolors.HEADER}[R]{bcolors.ENDC} Trying to GET {bcolors.BOLD}{filename}{bcolors.ENDC}")
 
-        if filename == '/' or filename == '':
-            res = open("static/index.html", encoding="utf8")
+        if filename.endswith('/') or filename == '':
+            res = open("static"+filename+"/index.html", encoding="utf8")
+        elif filename.endswith('.jpg') or filename.endswith('.png') or filename.endswith('.svg'):
+            res = open("static/"+filename[1:], 'rb')
+            conn.sendall((getHttpOKHeader()).encode() + res.read())
+            return
         else:
             res = open("static/"+filename[1:], encoding="utf8")
         print(f"{bcolors.OKCYAN}[R]{bcolors.ENDC} Sending {bcolors.OKGREEN}OK{bcolors.ENDC} Response back to {bcolors.OKCYAN}{ip}:{p}{bcolors.ENDC}")
@@ -20,9 +24,10 @@ def handleClientRequest(conn: socket, addrTuple):
         res = open("static/404.html", encoding="utf8")
         print(f"{bcolors.OKCYAN}[R]{bcolors.ENDC} Sending {bcolors.WARNING}Not Found{bcolors.ENDC} Response back to {bcolors.OKCYAN}{ip}:{p}{bcolors.ENDC}")
         conn.sendall((getHttpNotFoundHeader() + res.read() + "\r\n").encode())
-    except Exception:
+    except Exception as e:
         res = open("static/500.html", encoding="utf8")
         print(f"{bcolors.OKCYAN}[R]{bcolors.ENDC} Sending {bcolors.FAIL}Internal Server Error{bcolors.ENDC} Response back to {bcolors.OKCYAN}{ip}:{p}{bcolors.ENDC}")
+        print(f"{bcolors.FAIL}[ERROR]{bcolors.ENDC} : {e}")
         conn.sendall((getHttpInternalServerErrorHeader() + res.read() + "\r\n").encode())
     finally:
         conn.close()
